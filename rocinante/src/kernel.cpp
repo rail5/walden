501ec92f7899a6775d52b8835b590bc59d891a86
rail5 <andrew@rail5.org>
2026-02-26 23:11:12 +0800

    Commented explanations/descriptions of the a0,a1,a2 register contents in kernel_main

--- rocinante/src/kernel.cpp
+++ rocinante/src/kernel.cpp
@@ -24,14 +24,14 @@ static constinit Rocinante::Uart16550 uart(UART_BASE);
 
 } // namespace
 
-extern "C" [[noreturn]] void kernel_main(uint64_t a0, uint64_t a1, uint64_t a2) {
+extern "C" [[noreturn]] void kernel_main(uint64_t is_uefi_compliant_bootenv, uint64_t kernel_cmdline_ptr, uint64_t efi_system_table_ptr) {
 	Rocinante::Memory::InitEarly();
 
 	auto& cpucfg = Rocinante::GetCPUCFG();
 
 	Rocinante::String info;
 	info += "Hello, Rocinante!\n";
-	info += "Don the LoongArch64 armor and prepare to ride!\n";
+	info += "Don the LoongArch64 armor and prepare to ride!\n\n";
 
 	uart.write(info);
 
@@ -52,21 +52,47 @@ extern "C" [[noreturn]] void kernel_main(uint64_t a0, uint64_t a1, uint64_t a2) 
 			break;
 	}
 
+	uart.putc('\n');
+
 	if (cpucfg.MMUSupportsPageMappingMode()) {
 		uart.puts("MMU supports page mapping mode\n");
 	} else {
 		uart.puts("MMU does not support page mapping mode\n");
 	}
 
+	uart.putc('\n');
+
 	// Let's read and print VALEN/PALEN as a sanity check that our CPUCFG class is working and we can read CPU-reported information correctly.
 	uart.puts("Supported virtual address bits (VALEN): ");
 	uart.write(Rocinante::to_string(cpucfg.VirtualAddressBits()));
-	uart.puts("\n");
+	uart.putc('\n');
 	uart.puts("Supported physical address bits (PALEN): ");
 	uart.write(Rocinante::to_string(cpucfg.PhysicalAddressBits()));
-	uart.puts("\n");
+	uart.putc('\n');
+
+	uart.putc('\n');
 
 	// &c...
+
+	uart.puts("Contents of the a0 register (is_uefi_compliant_bootenv): ");
+	uart.write(Rocinante::to_string(is_uefi_compliant_bootenv));
+	uart.putc('\n');
+
+	uart.puts("Contents of the a1 register (kernel_cmdline_ptr): ");
+	if (kernel_cmdline_ptr) {
+		uart.puts(reinterpret_cast<const char*>(kernel_cmdline_ptr));
+	} else {
+		uart.puts("(null)");
+	}
+	uart.putc('\n');
+
+	uart.puts("Contents of the a2 register (efi_system_table_ptr): ");
+	if (efi_system_table_ptr) {
+		uart.write(Rocinante::to_string(efi_system_table_ptr));
+	} else {
+		uart.puts("(null)");
+	}
+	uart.putc('\n');
 
 	halt();
 }
--- rocinante/src/kernel.h
+++ rocinante/src/kernel.h
@@ -8,5 +8,17 @@
 #include <cstdint>
 
 extern "C" {
-	[[noreturn]] void kernel_main(uint64_t a0, uint64_t a1, uint64_t a2);
+	/**
+	 * @brief The main entry point of the kernel, called by the assembly language bootstrapping code after basic CPU setup is complete.
+	 *
+	 * @param is_uefi_compliant_bootenv Nonzero if the boot environment is UEFI-compliant, zero otherwise. Contents of the a0 register.
+	 * @param kernel_cmdline_ptr Pointer to a null-terminated ASCII string containing the kernel command line, or null if no command line was provided. Contents of the a1 register.
+	 * @param efi_system_table_ptr Pointer to the EFI system table. Contents of the a2 register.
+	 * 
+	 */
+	[[noreturn]] void kernel_main(
+		uint64_t is_uefi_compliant_bootenv /* a0 */,
+		uint64_t kernel_cmdline_ptr /* a1 */,
+		uint64_t efi_system_table_ptr /* a2 */
+	);
 }
