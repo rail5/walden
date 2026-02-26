1209f8e8792a4f30e3b2ad1acd31a808f5ad2d65
rail5 <andrew@rail5.org>
2026-02-26 19:33:09 +0800

    Basic CPUCFG read & display

--- rocinante/src/kernel.cpp
+++ rocinante/src/kernel.cpp
@@ -27,6 +27,32 @@ extern "C" [[noreturn]] void kernel_main(uint64_t a0, uint64_t a1, uint64_t a2) 
 	auto& cpucfg = Rocinante::GetCPUCFG();
 
 	uart.puts("Hello, Rocinante!\n");
+	uart.puts("Don the LoongArch64 armor and prepare to ride!\n");
+
+	// Print some information about the CPU configuration using the CPUCFG class
+	uart.puts("CPU Architecture: ");
+	switch (cpucfg.Arch()) {
+		case Rocinante::CPUCFG::Architecture::SimplifiedLA32:
+			uart.puts("Simplified LA32\n");
+			break;
+		case Rocinante::CPUCFG::Architecture::LA32:
+			uart.puts("LA32\n");
+			break;
+		case Rocinante::CPUCFG::Architecture::LA64:
+			uart.puts("LA64\n");
+			break;
+		case Rocinante::CPUCFG::Architecture::Reserved:
+			uart.puts("Reserved/Unknown\n");
+			break;
+	}
+
+	if (cpucfg.MMUSupportsPageMappingMode()) {
+		uart.puts("MMU supports page mapping mode\n");
+	} else {
+		uart.puts("MMU does not support page mapping mode\n");
+	}
+
+	// &c...
 
 	halt();
 }
